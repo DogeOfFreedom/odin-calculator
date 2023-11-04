fad7d116e2f21e16abab76bfc7b1e1ff61074fef
DogeOfFreedom <jchensi2000@gmail.com>
2023-11-04 18:13:27 +0800

    feat: button inputs display on calc display

--- script.js
+++ script.js
@@ -1,10 +1,50 @@
-const displayTextElement = document.querySelector(".display-text");
+const calcDisplay = document.querySelector(".display-text");
+const OPERATOR_VALUE = {
+    "^": 5,
+    "*": 4,
+    "/": 3,
+    "+": 2,
+    "-": 1
+}
 
 
 const updateDisplayText = (element) => {
-    let originalText = displayTextElement.textContent;
-    let btnId = element.id;
-    console.log(btnId);
+    let input = element.id;
+    
+    console.log(input);
+    if(!isNaN(input)) {
+        calcDisplay.textContent += input;
+    }
+    else if(input === "clear") {
+        calcDisplay.textContent = "";
+    } 
+    else if(input === "backspace") {
+        let len = calcDisplay.textContent.length;
+        calcDisplay.textContent = calcDisplay.textContent.substring(0, len-1);
+    }
+    else if(input === ".") {
+        if(!calcDisplay.textContent.includes(".")) {
+            calcDisplay.textContent += input;
+        }
+    }
+    else if(input === "equal") {
+        operate();
+    }
+    else { // Operators
+        calcDisplay.textContent += ` ${input} `;
+    }
+    
+}
+
+const operate = () => {
+    console.log("OPERATER OPERATER OP-OP-OP-OPERATER")
+}
+
+const addInputEventToButtons = () => {
+    let buttons = document.querySelectorAll(".btn-body button");
+    buttons.forEach(button => {
+        button.setAttribute("onclick", "updateDisplayText(this)")
+    })
 }
 
 const add = (n1, n2) => {
@@ -39,3 +79,4 @@ const operator = (op, n1, n2) => {
 let n1;
 let n2;
 let op;
+addInputEventToButtons();
