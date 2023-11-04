a36821024f7d06292ff54c6f6a4222811d4dd311
DogeOfFreedom <jchensi2000@gmail.com>
2023-11-04 22:36:33 +0800

    feat: add internal logic for evaluating expression
    Expressions are evaluated using the IDMAS convention

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 const calcDisplay = document.querySelector(".display-text");
 const errorMsg = document.querySelector("#error-msg");
-const OPERATOR_VALUE = {
-    "^": 5,
-    "*": 4,
-    "/": 3,
-    "+": 2,
-    "-": 1
-}
-
+const OPERATORS = ["^", "*", "/", "+", "-"];
+let clearPreviousResult = false;
 
 const updateDisplayText = (element) => {
     let input = element.id;
-    console.log(input);
+
+    if(clearPreviousResult) {
+        calcDisplay.textContent = "";
+        clearPreviousResult = false;
+    }
 
     if(!isNaN(input)) {
         calcDisplay.textContent += input;
@@ -29,24 +27,89 @@ const updateDisplayText = (element) => {
         }
     }
     else if(input === "equal") {
-        operate();
+        evaluate();
+        clearPreviousResult = true;
     }
     else if(input === "^") {
         let lastChar = calcDisplay.textContent.slice(-1).trim();
         if(!isNaN(lastChar) && lastChar !== "") {
             calcDisplay.textContent += input;
         } else {
-            errorMsg.textContent = "An exponent can only be used after a number";
+            errorMsg.textContent = 
+                "An exponent can only be used after a number";
         }
     }
     else { // All other operators
         calcDisplay.textContent += ` ${input} `;
     }
-    
 }
 
-const operate = () => {
-    console.log("OPERATER OPERATER OP-OP-OP-OPERATER");
+const evaluate = () => {
+    console.log("I'm operator mah");
+    let expressionInputs = 
+        calcDisplay.textContent.replace('^', ' ^ ').split(" ");
+    
+        console.table(expressionInputs);
+
+    if(!isValidexpression(expressionInputs)) {
+        errorMsg.textContent = "Invalid expression!";
+        return; 
+    }
+
+    calcDisplay.textContent = evaluateExpression(expressionInputs); 
+}
+
+const evaluateExpression = (expressionInputs) => {
+    let result = expressionInputs;
+    OPERATORS.forEach(operator => {
+        result = evaluateExpressionForOperator(operator, result);
+    })
+    return result.toString();
+}
+
+const evaluateExpressionForOperator = (op, expressionInputs) => {
+    if(expressionInputs.length <= 1) {
+        return expressionInputs;
+    }
+    let operatorExists = false; 
+    let left = [];
+    let evaluate = [];
+    let right = [];
+
+    for(let i = 0; i < expressionInputs.length; i++) {
+        if(expressionInputs[i] === op) {
+            let startSliceIndex = i-1; // Start is inclusive
+            let endSliceIndex = i+2; // End in exclusive
+            evaluate = expressionInputs.slice(startSliceIndex, endSliceIndex);
+            left = expressionInputs.slice(0, startSliceIndex);
+            right = expressionInputs.slice(endSliceIndex);
+            operatorExists = true;
+            break;
+        }
+    }
+
+    if(operatorExists) {
+        let n1 = Number(evaluate[0]);
+        let n2 = Number(evaluate[2]);
+        evaluate = [operator(op, n1, n2)];  
+
+        right = evaluateExpressionForOperator(op, right);
+
+        return left.concat(evaluate).concat(right);
+    } else {
+        return expressionInputs;
+    }    
+}
+
+const isValidexpression = (expression) => {
+    for(let i = 0; i < expression.length; i++) {
+        if(OPERATORS.includes(expression[i])) {
+            if(isNaN(expression[i-1]) && isNaN(expression[i+1])) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 const addInputEventToButtons = () => {
@@ -72,6 +135,10 @@ const divide = (n1, n2) => {
     return n1 / n2;
 }
 
+const pow = (n1, n2) => {
+    return Math.pow(n1, n2);
+}
+
 const operator = (op, n1, n2) => {
     switch (op) {
         case "+":
@@ -81,11 +148,10 @@ const operator = (op, n1, n2) => {
         case "*":
             return multiply(n1, n2);
         case "/":
-            return divide(n1, n2);             
+            return divide(n1, n2);   
+        case "^":
+            return pow(n1, n2);
     }
 }
 
-let n1;
-let n2;
-let op;
 addInputEventToButtons();
